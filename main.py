22a1a3092eeed530a35230a8353cecbc79e4f7e4
ankitkr23835 <72962795+ankitkr23835@users.noreply.github.com>
2024-04-14 16:01:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -636,6 +636,8 @@ async def download(event):
     global message
     global edit
     global max_retry
+    if dd % 6==0:
+        download_in_progress=False
     size=0
     if event.document:
         if event.document.size > 2500000000:
