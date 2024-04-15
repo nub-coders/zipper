ab6722d5834ea778cd72e4925db194ab794f93f1
ankitkr23835 <72962795+ankitkr23835@users.noreply.github.com>
2024-04-15 10:09:24 +0530

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import shutil
+import requests
+import hashlib
+from flask import Flask, send_file
+ggg=os.getcwd()
+app = Flask(__name__)
+
+
+@app.route("/<file_hash>")
+def download_zip(file_hash):
+    # Check file hash of all files in f"{ggg}/data directory
+    destination_dir = os.path.join(ggg, "userspace")
+    for file_name in os.listdir(destination_dir):
+        file_path = os.path.join(destination_dir, file_name)
+        if os.path.isfile(file_path):
+            with open(file_path, "rb") as f:
+                if hashlib.sha256(f.read()).hexdigest() == file_hash:
+                    return send_file(
+                        file_path,
+                        as_attachment=True)
+    return "File not found"
+
+if __name__ == "__main__":
+    app.run()
