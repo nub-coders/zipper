a2bd09f71b46e153d65d2d95cb2196876e8c0c03
u219967 <u219967@login-2.aidevcloud>
2024-05-12 14:55:06 -0700

    sexy

--- main.py
+++ main.py
@@ -748,7 +748,7 @@ async def download(event):
                 await msg.edit("Finished downloading\n/my_files to see your files")
             if fi is not None:
                 extension = os.path.splitext(fi)[1]  # Get
-                fi_encoded = fi.encode('utf-8')
+                fi_encoded = fi.encode('utf-8').decode('utf-8')
                 with open(f"{user_dir}/{fi_encoded}", "wb") as out:
                     try:
                      await asyncio.wait_for(download_file(event.client, docs, out, progress_callback=progress_bar), timeout=1800)
