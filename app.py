ab6e85c7190c4657f93434a1fb92a1de7c87d074
ankit <ankit>
2024-07-21 17:38:55 +0000

    fhd

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-import os
-import subprocess
-import shutil
-import requests
-import hashlib
-from flask import Flask, send_file
-ggg=os.getcwd()
-app = Flask(__name__)
-subprocess.Popen(['ssh', '-R', 'https://justtesting-5055c1719887.herokuapp.com:8080:localhost:8080', 'serveo.net'])
-@app.route("/")
-def home():
-    return "nothing here"
-@app.route("/<file_hash>")
-def download_zip(file_hash):
-    # Check file hash of all files in f"{ggg}/data directory
-    destination_dir = os.path.join(ggg, "userspace")
-    for file_name in os.listdir(destination_dir):
-        file_path = os.path.join(destination_dir, file_name)
-        if os.path.isfile(file_path):
-            with open(file_path, "rb") as f:
-                if hashlib.sha256(f.read()).hexdigest() == file_hash:
-                    return send_file(
-                        file_path,
-                        as_attachment=True)
-    return "File not found"
+from flask import Flask, session, request, render_template, redirect, url_for, send_file
 
-if __name__ == "__main__":
-    app.run()
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def index():
+    return 'index.html'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000, debug=True)
