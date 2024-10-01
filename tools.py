6e75d6b1bf6564443db0e29b13b4c9a5f334e48c
Ubuntu <ubuntu@ip-172-31-45-227.ap-south-1.compute.internal>
2024-10-01 16:48:02 +0000

    fhdh

--- tools.py
+++ tools.py
@@ -19,7 +19,11 @@ async def is_user_on_chat(bot: TelegramClient, chat_id: int, user_id: int) -> bo
     """
     try:
         botting = bot_permissions = await bot.get_permissions(chat_id, bot._self_id)
+    except:
+        return True
+    try:
         check = await bot.get_permissions(chat_id, user_id)
         return check
     except:
-        return False
+       return False
+
