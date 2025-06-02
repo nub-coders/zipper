1f6f32d886b61f2f05ff8e45796a57642194caad
NUB <nubcoders@gmail.com>
2025-06-02 21:53:30 +0530

    updated bot token

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import queue
 API_ID = 21869707
 API_HASH = '31ec80a4adad7aaad9262e894e3654e6'
 BOT_TOKEN='7571416784:AAEJJK9bINObGk96VuC6JLR2CzwclVUOXbE'
-
+BOT_TOKEN='6239906461:AAFrz8NvMpG5o9oXGIx_XDEl34ulTK18wtY'
 # Global variables
 collection = None
 ggg = os.getcwd()
